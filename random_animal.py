b649d34973b7b0df594b22de2a27ae1386387712
NH <moltenrock@gmail.com>
2018-10-01 22:56:40 +0300

    add nh to contributors list

---
+++ random_animal.py
@@ -0,0 +1,10 @@
+import random
+
+input("guess which animal is coming next?")
+
+MyList = ['dog','cat','snake','rabbit']
+
+print ("and your animal is...")
+print(random.choice(MyList))
+
+print("Thanks for playing the animal game!")
